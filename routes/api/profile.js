97f9d91eb22d28eeae4db72645f2990026ee7153
Huseyin <huseyinkarakaya006@gmail.com>
2022-11-01 14:50:06 +0300

    Profile Model

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,94 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const { check, validationResult } = require('express-validator');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
 
-// @route   GET api/profile
-// @desc    Test Route
-// @access  Public
-router.get('/', (req, res) => res.send('Profile route'));
+// @route   GET api/profile/me
+// @desc    Get Current Users Profile
+// @access  Private
+router.get('/me', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id }).populate(
+      'user',
+      ['name', 'avatar']
+    );
+
+    if (!profile) {
+      return res.status(400).json({ msg: 'There is no profile for this user' });
+    }
+    res.json(profile);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route   POST api/profile
+// @desc    Create or Update User Profile
+// @access  Private
+router.post(
+  '/',
+  [
+    auth,
+    [
+      check('status', 'Status is required').not().isEmpty(),
+      check('skills', 'Skills is required').not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {
+      company,
+      website,
+      location,
+      bio,
+      status,
+      githubusername,
+      skills,
+      youtube,
+      facebook,
+      twitter,
+      instagram,
+      linkedin,
+    } = req.body;
+
+    const profilefields = {};
+    profilefields.user = req.user.id;
+    if (company) profilefields.company = company;
+    if (website) profilefields.website = website;
+    if (location) profilefields.location = location;
+    if (bio) profilefields.bio = bio;
+    if (status) profilefields.status = status;
+    if (githubusername) profilefields.githubusername = githubusername;
+    if (skills) {
+      profilefields.skills = skills.split(',').map((skill) => skill.trim());
+    }
+
+    console.log(profilefields.skills);
+    res.send('Hello');
+
+    profilefields.social = {};
+    if (youtube) profilefields.social.youtube = youtube;
+    if (twitter) profilefields.social.youtube = youtube;
+    if (facebook) profilefields.social.facebook = facebook;
+    if (linkedin) profilefields.social.linkedin = linkedin;
+    if (instagram) profilefields.social.instagram = instagram;
+
+    try {
+      let profile = Profile.findOne({ user: req.user.id });
+
+      // ABI BURDAN DEVAM ET
+    } catch (error) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
 
 module.exports = router;

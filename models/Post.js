55dec0ce4cf02c76991128b3661bd75de6251bab
Huseyin <huseyinkarakaya006@gmail.com>
2022-12-10 23:50:01 +0300

    Post Route created

--- models/Post.js
+++ models/Post.js
@@ -1,5 +1,5 @@
-const mongoose = require('mongooese');
-const Schema = mongooese.Schema;
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
 const PostSchema = new Schema({
   user: {
@@ -52,4 +52,4 @@ const PostSchema = new Schema({
   },
 });
 
-module.exports = Post = mongooese.model('post', Post);
+module.exports = Post = mongoose.model('post', PostSchema);

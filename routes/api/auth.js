02c6a16ce0e54b5d6e9b7a3c6619ba8cf4bc2150
mert <smilinggreenfrog31@gmail.com>
2023-05-23 14:45:46 +0300

    auth.js updated 2 factor

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -5,9 +5,12 @@ const { check, validationResult } = require('express-validator');
 const config = require('config');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
+const nodemailer = require('nodemailer');
+const crypto = require('crypto');
 
 const User = require('../../models/User');
 const Company = require('../../models/Company');
+
 
 // @route   GET api/auth
 // @desc    Test Route
@@ -32,6 +35,37 @@ router.get('/', auth, async (req, res) => {
   }
 });
 
+// @route   POST api/auth/verify-email
+// @desc    Verify user's email
+// @access  Public
+router.post('/verify-email', async (req, res) => {
+  const { email, verificationCode } = req.body;
+
+  try {
+    // Find the user by email
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({ msg: 'User not found' });
+    }
+
+    // Check if the provided verification code matches the stored code
+    if (verificationCode !== user.verificationCode) {
+      return res.status(400).json({ msg: 'Invalid verification code' });
+    }
+
+    // Update the user's status to "verified"
+    user.isVerified = true;
+    await user.save();
+
+    return res.json({ msg: 'Email verification successful' });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+
 // @route   POST api/auth
 // @desc    Authenticate engineer or company & Get token
 // @access  Public
@@ -46,7 +80,9 @@ router.post(
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-
+    if (!user.isVerified) {
+      return res.status(400).json({ errors: [{ msg: 'Email not verified' }] });
+    }
     const { email, password } = req.body;
     try {
       let user = await User.findOne({ email });
@@ -119,5 +155,72 @@ router.post(
     }
   }
 );
+
+// @route   POST api/auth/register
+// @desc    Register user
+// @access  Public
+router.post(
+  '/register',
+  [
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, password } = req.body;
+
+    try {
+      // Check if the email is already registered
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return res.status(400).json({ errors: [{ msg: 'Email is already registered' }] });
+      }
+
+      // Generate a random 6-digit verification code
+      const verificationCode = Math.floor(100000 + Math.random() * 900000).toString();
+
+      // Create a new user instance
+      user = new User({
+        name,
+        email,
+        password,
+        verificationCode,
+      });
+
+      // Encrypt the password
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+
+      // Save the user to the database
+      await user.save();
+
+      // Send verification email to the user's registered email address
+      const transporter = nodemailer.createTransport({
+        // Configure the email transporter settings (e.g., SMTP)
+      });
+
+      const mailOptions = {
+        from: 'your_email@example.com',
+        to: email,
+        subject: 'Email Verification',
+        text: `Your verification code is: ${verificationCode}`,
+      };
+
+      await transporter.sendMail(mailOptions);
+
+      return res.json({ msg: 'User registered successfully. Verification code sent to your email' });
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
+
 
 module.exports = router;
